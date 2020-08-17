ff7354d18942573a3db01f9fac7e91a807705065
mgbedorochinedu <mgbedorochinedu@gmail.com>
2020-08-17 06:14:20 -1200

    Edited nodemon to work with teams

--- src/util/forecast.js
+++ src/util/forecast.js
@@ -9,9 +9,7 @@ const forecast = (latitude, logtitude,  callback) => {
             callback("Unable to connect to weather service", undefined)
         } else if(body.error){
             callback("Unable to find location", undefined)
-        } else{
-            console.log()
-          
+        } else{          
             callback(undefined, 
                 body.current.observation_time + ": " + "It is " + body.current.weather_descriptions + " and currently " + body.current.temperature + " degree out. There is a " + body.current.precip + "% chance of rain."
             )
